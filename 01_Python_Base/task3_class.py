be2d11470431b619104220ca3e226766bf3d608f
jiayinglili <jiaying_li1107@163.com>
2025-11-30 21:19:26 +0800

    imageprocessor

---
+++ 01_Python_Base/task3_class.py
@@ -0,0 +1,15 @@
+class ImageProcessor:
+    def __init__(self,name):
+        self.name = name
+        self.size = 0
+    def process_image(self):
+        self.size = 1024
+    def show_info(self):
+        print(self.name,self.size)
+def main():
+    processor = ImageProcessor("img1")
+    processor.show_info()
+    processor.process_image()
+    processor.show_info()
+main()
+
